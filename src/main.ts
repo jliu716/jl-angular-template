0062871d8bf75c8426c11304733d94bd5299861d
Jiayi Liu <liujiayi19920724@gmail.com>
2025-03-08 15:07:34 +1300

    install fontsource

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import "@fontsource/roboto";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/200.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/600.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/800.css";
+import "@fontsource/roboto/900.css";
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
