e1cb1e91be3a9264e7d40b260efa81525289c05d
Jiayi Liu <liujiayi19920724@gmail.com>
2025-03-23 12:25:27 +1300

    jss test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+      path: '',
+      title: 'jss demo',
+      component: HomeComponent
+   }
+];
